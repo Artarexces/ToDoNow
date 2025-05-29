11bfe3c309b921e7761180eba71515adc8fac970
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-05-29 00:30:14 -0300

    views & urls + templates

---
+++ tasks/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('register/', views.register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='tasks/login.html')),
+    path('logout/', views.logout_view, name='logout'),
+    path('tasks/create/', views.task_create, name='task_create'),
+    path('tasks/<int:pk>/edit/', views.task_edit, name='task_edit'),
+    path('tasks/<int:pk>/complete/', views.task_complete, name='task_complete'),
+    path('tasks/<int:pk>/delete', views.task_delete, name='task_delete'),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,94 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import login, aauthenticate, logout
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.contrib import messages
 from .forms import CustomUserCreationForm, TaskForm
 from .models import Task
 
 
+## Creacion de usuarios
 
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Usuario registrado exitosamente.')
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'tasks/register.html', {'form': form})
+
+
+
+## Inicio y cierre de session (login-logout)
+
+class UserLoginView(LoginView):
+    template_name = 'tasks/login.html'
+    redirect_authenticated_user = True
+    
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+## Tareas 
+
+@login_required(login_url='login')
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user).order_by('-created_at')
+    return render (request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+## Creacion de tarea
+
+@login_required(login_url='login')
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_list')
+        
+    else:
+        form = TaskForm()
+    return render (request, 'tasks/task_form.html', {'form': form})
+
+
+## Editar tarea 
+
+@login_required(login_url='login')
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+        
+    else:
+        form = TaskForm(instance=task)
+    return render (request, 'tasks/task_form.html', {'form': form, 'title':'Editar tarea'})
+
+
+## Completar tarea
+
+@login_required(login_url='login')
+def task_complete(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    task.completed = True
+    task.save()
+    return redirect('task_list')
+
+
+## Eliminar tarea
+
+@login_required(login_url='login')
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    task.delete()
+    return redirect('task_list')
